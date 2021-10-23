bf216ef7f9e69fb1951020b95275d59e9661c933
reilly-m <reilly_m@eircom.net>
2021-10-23 02:24:04 +0100

    Committ 5

--- main.py
+++ main.py
@@ -1,32 +1,68 @@
 import pandas as pd
 import os
+import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from numpy.random import randn
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1500)
 
-# A function to list the files and directories
-def list_full_paths(directory):
-    return [os.path.join(directory, file) for file in os.listdir(directory)]
 
-# Import the csv files and load into a Dataframe
-filepaths = list_full_paths('./UK Used Cars')
-df = pd.concat(map(pd.read_csv, filepaths))
+# Import Car Data - 9 files
+files = glob.glob('UK Used Cars/*.csv')
+df = pd.concat([pd.read_csv(fp).assign(make=os.path.basename(fp)) for fp in files])
 
-# Print the first 10 lines
-print(df.head())
+# Add make as column
+df['make'] = df['make'].str.rstrip('.csv')
 
-# Descibe the Dataset
+# Drop duplicates and Reset Index
+df.drop_duplicates(subset=None, keep='first', inplace=True, ignore_index=True)
+
+
+# Copy tax(£) to tax for hyundai
+df.loc[df['make'] == 'hyundi', 'tax'] = df['tax(£)']
+
+# Amend the zero engine sizes
+df['engineSize'] = df['engineSize'].replace(0,1)
+
+# Drop tax(£) column
+df.drop(['tax(£)'],axis = 1, inplace=True)
+
+# Describe Data
+
+#pd.set_option('display.max_columns', None)
+#print(df.head())
+print(df.shape)
 print(df.info)
 print(df.describe())
+print(df.isnull().sum())
+print(df.columns)
 
-# Supervised Learning / Regression Model
-# Checking for multicolinerality
-# Scalar
-# Score function
-# knn
-# decision tree
-# Hyperprameter
-# values count
-# data.describe()
-# decision tree classifer
-# Hyper Parameter
-# Clean the Data
+# Data Visualisation
+df_sample = df.sample(n=200,random_state=44)
+print(df_sample.shape)
+sns.set_style('dark')
+# mpg v price
+sns.regplot(x='mpg',y='price',data=df_sample,ci=None, scatter_kws={'s':100,'alpha':0.5,'color':'blue'},
+line_kws={'lw':4,'color':'black','linestyle':'-.'})
+plt.legend
+plt.show()
+# tax v price
+sns.regplot(x='tax',y='price',data=df_sample,ci=None, scatter_kws={'s':100,'alpha':0.5,'color':'blue'},
+line_kws={'lw': 4,'color':'black','linestyle':'-.'})
+plt.legend
+plt.show()
+fuel_map={'Diesel':1,
+         'Electric':2,
+         'Hybrid':3,
+         'Other':4,
+         'Petrol':5}
+df_sample['fuel_value'] = df_sample.fuelType.map(fuel_map)
+print(df_sample.fuel_value.value_counts())
+sns.regplot(x='fuel_value',y='price',data=df_sample, x_estimator=np.mean)
+plt.legend
+plt.show()
 
-
+# Basic Model - Linear Regression
